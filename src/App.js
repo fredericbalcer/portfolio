ddf0297cb096fc30862ba091fcff588a7642530e
Frederic.Balcer <frederic.balcer@skynet.be>
2026-01-02 23:48:19 +0100

    Version initiale s Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Cv from './pages/Cv'
+import Skills from './pages/Skills'
+import Contact from './pages/Contact'
+import { Box } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Box sx={{ mt: 8 }}>
+        <Routes>
+          <Route path="/" element={<Cv />} />
+          <Route path="/cv" element={<Cv />} />          
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Box>
+    </Router>
+  )
 }
 
-export default App;
+export default App
